69b3558415ea522e0e9068341d715e72637dd0d2
Francisco Alves <fjunior.alves.oliveira@gmail.com>
2020-08-28 16:27:00 -0300

    Altera variaveis de ambiente para CKAN producao

--- scripts/publish.R
+++ scripts/publish.R
@@ -6,7 +6,7 @@ id <- datapackage$resources %>% purrr::map_chr("id")
 output <- datapackage$resources %>% purrr::map_chr("path")
 
 res <- purrr::map2(id, output, ckanr::resource_update, 
-                  url = Sys.getenv("DADOSMG_DEV_HOST"), 
-                  key = Sys.getenv("DADOSMG_DEV"))
+                  url = Sys.getenv("DADOSMG_PROD_HOST"), 
+                  key = Sys.getenv("DADOSMG_PROD"))
 
 map2(output, purrr::map(res, "url"), check_upload)
